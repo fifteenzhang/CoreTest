6fffdc31fb7b93770429731a3efb1cc647ed41c1
张璘 <zhanglin@tuhu.cn>
2018-08-10 10:00:23 +0800

    CoreTest Init

---
+++ demo/CoreTest/Demo/Demo/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Controllers
+{
+    public class TestController : ControllerBase
+    {
+        //[Route("[Action]")]
+        public object Hello()
+        {
+            return Ok(new { Status = true, Data = "Hello", Msg = string.Empty });
+        }
+
+        public object Hello2()
+        {
+            return Ok(new { Status = true, Data = "Hello", Msg = string.Empty });
+        }
+    }
+}
